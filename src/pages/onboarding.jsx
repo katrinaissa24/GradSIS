aa586601d6806e06867c6da426a7522e58329b5d
jadakoum <jadalakoum@gmail.com>
2026-03-23 18:25:58 +0200

    small fix

--- src/pages/onboarding.jsx
+++ src/pages/onboarding.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { supabase } from "./services/supabase";
+import { supabase } from "../services/supabase";
 import { useNavigate } from "react-router-dom";
 
 // Custom Dropdown Component
